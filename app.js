9b36fed658bbece2c1e42769d62ae7023c1430bc
Vasilina Biznyakova <vbiznyakova@gmail.com>
2022-12-26 22:40:17 +0100

    add mongdb

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+// connected DB
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+
+const DB_HOST =
+  "mongodb+srv://Vas:MlrKfr9bD6UmNSUe@cluster0.epi5ilu.mongodb.net/db_contacts?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connection success"))
+  .catch((error) => console.log(error.message));
+
+// connected DB
 
 const contactsRouter = require("./routes/api/contacts");
 
